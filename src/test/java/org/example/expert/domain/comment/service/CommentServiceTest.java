602a38d137102c87cf4472d8e123aa11138c235f
hugecookie <phrpp5770@gmail.com>
2025-04-16 13:44:44 +0900

    refactor: Lv 3 - 테스트코드 연습
    ## 변경사항
    - PasswordEncoderTest: 인자 순서 오류 수정
    - CommentServiceTest: 할일이 존재하지 않을 경우의 예외 테스트 수정
    - ManagerServiceTest: Todo 미존재 시 메시지 수정 및 테스트 메서드명 변경
    - UserAdminService: 유저 권한 변경 로직 수정
    
    ## 이유
    - 인자 순서 오류로 인해 password match 테스트가 실패하던 문제를 해결
    - 존재하지 않는 Todo에 대한 예외가 ServerException이 아니라 InvalidRequestException을 던지도록 컨텍스트 맞춤
    - Manager 조회 시 메시지가 요구사항과 달라 테스트 실패, 이를 맞게 수정하고 메서드명도 컨텍스트에 맞게 변경
    - 사용자 권한 변경 기능 구현을 위한 서비스 로직 작성

--- src/test/java/org/example/expert/domain/comment/service/CommentServiceTest.java
+++ src/test/java/org/example/expert/domain/comment/service/CommentServiceTest.java
@@ -5,6 +5,7 @@ import org.example.expert.domain.comment.dto.response.CommentSaveResponse;
 import org.example.expert.domain.comment.entity.Comment;
 import org.example.expert.domain.comment.repository.CommentRepository;
 import org.example.expert.domain.common.dto.AuthUser;
+import org.example.expert.domain.common.exception.InvalidRequestException;
 import org.example.expert.domain.common.exception.ServerException;
 import org.example.expert.domain.todo.entity.Todo;
 import org.example.expert.domain.todo.repository.TodoRepository;
@@ -36,19 +37,16 @@ class CommentServiceTest {
     @Test
     public void comment_등록_중_할일을_찾지_못해_에러가_발생한다() {
         // given
-        long todoId = 1;
-        CommentSaveRequest request = new CommentSaveRequest("contents");
-        AuthUser authUser = new AuthUser(1L, "email", UserRole.USER);
+        long todoId = 1L;
+        AuthUser authUser = new AuthUser(1L, "email@email.com", UserRole.USER);
+        CommentSaveRequest request = new CommentSaveRequest("댓글입니다");
 
-        given(todoRepository.findById(anyLong())).willReturn(Optional.empty());
+        given(todoRepository.findById(todoId)).willReturn(Optional.empty());
 
-        // when
-        ServerException exception = assertThrows(ServerException.class, () -> {
+        // when & then
+        assertThrows(InvalidRequestException.class, () -> {
             commentService.saveComment(authUser, todoId, request);
         });
-
-        // then
-        assertEquals("Todo not found", exception.getMessage());
     }
 
     @Test
@@ -69,5 +67,6 @@ class CommentServiceTest {
 
         // then
         assertNotNull(result);
+        assertEquals("contents", result.getContents());
     }
 }
